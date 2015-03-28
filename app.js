b27788dd0e2829bacb4e7f9e828a3db3613e1b4f
Bonsanto <xbonsx@gmail.com>
2015-03-28 15:51:51 -0430

    - package.json for the node dependencies. - app.js for the main server app.

---
+++ app.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Bonsanto on 3/28/2015.
+ */
+var bodyParser = require("body-parser"),
+	express = require("express"),
+	fs = require("fs"),
+	http = require("http"),
+	path = require("path"),
+	url = require("url");
