79b9b3572f7782036d0ca32c85cc5b674dedecf7
Bonsanto <xbonsx@gmail.com>
2015-04-01 21:53:55 -0430

    -idea

--- app.js
+++ app.js
@@ -1,9 +1,54 @@
-/**
- * Created by Bonsanto on 3/28/2015.
- */
-var bodyParser = require("body-parser"),
+var http = require('http'),
+	fs = require('fs'),
 	express = require("express"),
-	fs = require("fs"),
-	http = require("http"),
-	path = require("path"),
-	url = require("url");
+	multer = require("multer"),
+	favio = require("./libs");
+
+var app = express(),
+	done = false,
+	port = 1337;
+
+app.get("/", function (req, res) {
+	//res.writeHead(200, {
+	//});
+	res.sendFile("index.html", {
+		root: __dirname + "/views/"
+	}, function (error) {
+		if (error) {
+			res.status(error.status).end();
+			console.log(error);
+		}
+	});
+});
+
+app.post("/", function (req, res) {
+	if (done) {
+		console.log(req.files);
+		res.end("File uploaded.");
+	}
+});
+
+app.listen(port, function () {
+	console.log("Working on port " + port);
+});
+
+/**
+ * res.writeHead(200, {'Content-Type': 'text/html'});
+ * res.write('<html><body><img src="data:image/jpeg;base64,')
+ * res.write(new Buffer(data).toString('base64'));
+ * res.end('"/></body></html>');
+ * */
+
+/**
+ fs.readFile('image.jpg', function (err, data) {
+	if (err) throw err; // Fail if the file can't be read.
+	http.createServer(function (req, res) {
+		favio("Favio");
+		res.writeHead(200, {
+			'Content-Type': 'image/jpeg'
+		});
+		res.end(data); // Send the file data to the browser.
+	}).listen(1337);
+	console.log('Server running at http://localhost:8124/');
+});
+ **/
